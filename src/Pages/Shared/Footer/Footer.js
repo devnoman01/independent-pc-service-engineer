107231fb0fd3a09f19cd2ec8e4918be3c899852f
devnoman01 <devnoman01@gmail.com>
2022-04-18 13:27:28 +0600

    footer part complete

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDribbble,
+  faFacebook,
+  faGithub,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
 import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="py-4">
+    <footer>
       <div className="container footer">
-        <div className="d-flex mx-auto text-center gap-2">
+        <div className="d-flex mx-auto my-2 text-center gap-2">
           <div className="col">
             <Link className="footer-link" to="/home">
               Home
@@ -23,7 +32,7 @@ const Footer = () => {
             </Link>
           </div>
           <div className="col">
-            <Link className="footer-link" to="/blog">
+            <Link className="footer-link" to="/blogs">
               Blogs
             </Link>
           </div>
@@ -33,10 +42,27 @@ const Footer = () => {
             </Link>
           </div>
         </div>
-        <div className="d-flex mx-auto text-center gap-2">
+        <div className="icon-row d-flex mx-auto text-center">
           <div className="col">
-            <img src="" alt="" />
+            <FontAwesomeIcon className="footer-icon" icon={faFacebook} />
           </div>
+          <div className="col">
+            <FontAwesomeIcon className="footer-icon" icon={faInstagram} />
+          </div>
+          <div className="col">
+            <FontAwesomeIcon className="footer-icon" icon={faTwitter} />
+          </div>
+          <div className="col">
+            <FontAwesomeIcon className="footer-icon" icon={faGithub} />
+          </div>
+          <div className="col">
+            <FontAwesomeIcon className="footer-icon" icon={faDribbble} />
+          </div>
+        </div>
+        <div className="row text-center">
+          <p className="footer-text">
+            &copy; 2020 PC Hardware Man. All right reserved
+          </p>
         </div>
       </div>
     </footer>
--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,15 +1,19 @@
 import React from "react";
+import Footer from "../Footer/Footer";
 import "./NotFound.css";
 
 const NotFound = () => {
   return (
-    <div className="container">
-      <div className="not-found-div d-flex flex-column align-items-center">
-        <h1 className="error-404">404</h1>
-        <h1 className="error-message">Page Not Found</h1>
+    <div className="">
+      <div className="container not-found-div d-flex flex-column align-items-center">
+        <h1 className="error-404 text-center">404</h1>
+        <h1 className="error-message text-center">Page Not Found</h1>
         <p className="text-message text-center">
           Please try to find pages from menubar
         </p>
+      </div>
+      <div className="fixed-bottom">
+        <Footer></Footer>
       </div>
     </div>
   );
